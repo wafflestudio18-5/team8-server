fe30cf65c505a78712bfb17c37adf98aeaf2c66b
Dohyeon Park <dodo4114@naver.com>
2020-12-04 21:21:54 +0900

    Create Profile Model (#6)

--- podo_server/podo_app/models.py
+++ podo_server/podo_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class TimeModel(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
@@ -6,3 +7,7 @@ class TimeModel(models.Model):
   class Meta:
     abstract = True
 
+class Profile(TimeModel):
+  user = models.ForeignKey(User, related_name='profile',on_delete=models.CASCADE)
+  nickname = models.CharField(max_length=100)
+  temperature = models.FloatField(default=36.5)
