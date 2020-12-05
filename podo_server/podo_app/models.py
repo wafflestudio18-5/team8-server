93d6726e537de81e60ae2ec376dc23fa2d5dad5f
Dohyeon Park <dodo4114@naver.com>
2020-12-05 20:33:12 +0900

    Create City and ProfileCity models(#7)

--- podo_server/podo_app/models.py
+++ podo_server/podo_app/models.py
@@ -11,3 +11,12 @@ class Profile(TimeModel):
   user = models.ForeignKey(User, related_name='profile',on_delete=models.CASCADE)
   nickname = models.CharField(max_length=100)
   temperature = models.FloatField(default=36.5)
+
+class City(TimeModel):
+  name = models.CharField(max_length=100)
+  location = models.CharField(max_length=100)
+
+class  ProfileCity(TimeModel):
+  profile = models.ForeignKey(Profile, related_name='profile_cities',on_delete=models.CASCADE)
+  city = models.ForeignKey(City, related_name='profile_cities',on_delete=models.CASCADE)
+  
