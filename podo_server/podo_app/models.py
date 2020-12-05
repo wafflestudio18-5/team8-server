e49461861fceb05f7fc9df7fa090e2cd387dae9c
Dohyeon Park <dodo4114@naver.com>
2020-12-05 20:28:38 +0900

    Create Product and LikeProduct models(#8)

--- podo_server/podo_app/models.py
+++ podo_server/podo_app/models.py
@@ -20,3 +20,22 @@ class  ProfileCity(TimeModel):
   profile = models.ForeignKey(Profile, related_name='profile_cities',on_delete=models.CASCADE)
   city = models.ForeignKey(City, related_name='profile_cities',on_delete=models.CASCADE)
   
+class Product(TimeModel):
+  name = models.CharField(max_length=100)
+  category = models.CharField(max_length=50)
+  price = models.PositiveIntegerField()
+  allow_suggest = models.BooleanField()
+  status = models.CharField(max_length=50)
+  seller = models.ForeignKey(Profile, related_name='selling_products',on_delete=models.SET_NULL, null=True)
+  buyer = models.ForeignKey(Profile, related_name='bought_products',on_delete=models.SET_NULL, null=True)
+  
+  count_comments = models.PositiveSmallIntegerField(default=0)
+  count_likes = models.PositiveSmallIntegerField(default=0)
+  count_views = models.PositiveSmallIntegerField(default=0)
+
+  city = models.ForeignKey(City, related_name='here_products',on_delete=models.SET_NULL, null=True)
+  distance_range = models.PositiveSmallIntegerField(default=0)
+
+class LikeProduct(TimeModel):
+  profile = models.ForeignKey(Profile, related_name='like_products',on_delete=models.SET_NULL, null=True)
+  product = models.ForeignKey(Product, related_name='like_profiles',on_delete=models.SET_NULL, null=True)
