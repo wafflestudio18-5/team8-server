0f34cfc82e815d7ae3b9342f17c96ecbaefdb448
Dohyeon Park <dodo4114@naver.com>
2020-12-05 19:44:25 +0900

    Set Database as MySQL

--- podo_server/podo_server/settings.py
+++ podo_server/podo_server/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'podo_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'NAME': 'podo_db',
+        'USER': 'waffle-backend',
+        'PASSWORD': 'seminar',
     }
 }
 
