de8d8e4dc96edffd79a505172345b256712ab085
Dohyeon Park <dodo4114@naver.com>
2020-12-26 02:20:11 +0900

    Set ping-pong

--- podo_server/podo_server/urls.py
+++ podo_server/podo_server/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+from podo_server.views import ping
+
 urlpatterns = [
+    path('', ping),
     path('admin/', admin.site.urls),
 ]
