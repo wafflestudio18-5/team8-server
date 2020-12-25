382cefd5de223d2b1028db3babfb1560cd0bd039
Dohyeon Park <dodo4114@naver.com>
2020-12-26 02:20:11 +0900

    Set ping-pong

--- podo_server/podo_server/urls.py
+++ podo_server/podo_server/urls.py
@@ -19,6 +19,9 @@ from django.urls import path, include
 
 from podo_server.views import ping
 
+
+from podo_server.views import ping
+
 urlpatterns = [
     path('', ping),
     path('api/v1/', include('user.urls')),
