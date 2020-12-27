5f786904e39c8ce0dd11b675c7cfbe034c8e5fbc
elijah0430 <68674522+elijah0430@users.noreply.github.com>
2020-12-27 17:22:40 +0900

    recovery-2

--- podo_server/user/serializer.py
+++ podo_server/user/serializer.py
@@ -18,3 +18,34 @@ class UserSerializer(serializers.ModelSerializer):
             'last_login',
             'date_joined',
         )
+
+class ProfileSerializer(serializers.ModelSerializer):
+    nickname = serializers.CharField(required=False)
+#add this to design
+    image = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = (
+            'nickname',
+            'image',
+        )
+
+class UserAndProfileSerializer(serializers.ModelSerializer):
+    user_id=serializers.IntegerField(required=True)
+    full_name = serializers.CharField(required=False)
+    nickname = serializers.CharField(required=False)
+    image=serializers.URLField(required=False)
+    class Meta:
+        model=Profile
+        fields=(
+            "user_id",
+            "full_name",
+            "nickname", 
+            "image", 
+            "temperature",
+#            "badges",
+#            "Products bought",
+#            "Products sold",
+        )
+
