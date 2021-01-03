aa4b2ff4081995e6ce85635994d1956225f29db7
DohyeonPark <dodo4114@naver.com>
2021-01-03 23:47:58 +0900

    Configure podo_app.admin

--- podo_server/podo_app/admin.py
+++ podo_server/podo_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from podo_app.models import Profile, City, ProfileCity, Product, LikeProduct, ChatRoom, Message
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(City)
+admin.site.register(ProfileCity)
+admin.site.register(Product)
+admin.site.register(LikeProduct)
+admin.site.register(ChatRoom)
+admin.site.register(Message)
