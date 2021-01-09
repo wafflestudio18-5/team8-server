e512b9992571ac7830922bc61f856eee73134b07
DohyeonPark <dodo4114@naver.com>
2021-01-09 15:26:21 +0900

    Hotfix

--- podo_server/podo_app/admin.py
+++ podo_server/podo_app/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from podo_app.models import Profile, City, ProfileCity, Product, LikeProduct, ChatRoom
+from podo_app.models import *
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(City)
 admin.site.register(ProfileCity)
 admin.site.register(Product)
+admin.site.register(ProductImage)
 admin.site.register(LikeProduct)
 admin.site.register(ChatRoom)
